e3e63dcafaaa90a40dc2fa32ca7dff79298be730
Neurizinn <neuridelima1@gmail.com>
2024-11-13 18:45:04 -0300

    delete

--- API/controllers/deleteController.js
+++ API/controllers/deleteController.js
@@ -1,13 +1,21 @@
 const db = require('../database');
 
 exports.removerItem = (req, res) => {
-    console.log(req.body);
-    // const itemId = req.params.id;
-    // console.log(`Removendo item com ID: ${itemId}`);
+    const itemID = req.body.id;
+    const itemTipo = req.body.tipo;
+    const sql = `DELETE FROM ${itemTipo} WHERE id= ?`;
+    let sucesso;
 
+    db.run(sql, [itemID], function(err) {
+        if (err) {
+            console.error('Erro ao remover item:', err.message);
+            sucesso = false
+        } else {
+            console.log(`Item removido com sucesso!`);
+            sucesso = true
+        }
+    });
     
-
-    const sucesso = true; // Suponha que a remoção foi bem-sucedida
 
     if (sucesso) {
         res.status(200).json({ mensagem: 'Item removido com sucesso!' });
